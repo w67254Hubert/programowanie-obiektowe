8198adddb2e2465f21baddbbc1a912d8f93fa0fc
w67254Hubert <w67254@student.wsiz.edu.pl>
2023-11-22 12:43:59 +0100

    dział

---
+++ LAB1/Lab2/Models/Licz.cs
@@ -0,0 +1,17 @@
+﻿namespace Lab2.Models
+{
+    public class Licz
+    {
+        public int Value;
+
+        public void Dodaj(int add)
+        {
+            Value += add;
+        }
+
+        public void Oddejmnij(int sub)
+        {
+            Value -= sub;
+        }
+    }
+}
