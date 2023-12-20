cada24f011ac460a8296dd33b58b8274d80e7586
w67254Hubert <w67254@student.wsiz.edu.pl>
2023-12-20 14:00:09 +0100

    kon

---
+++ LAB1/WebApplication3/Controllers/PersonControler.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication3.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PersonControler : ControllerBase
+    {
+        // GET: api/<PersonControler>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<PersonControler>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PersonControler>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PersonControler>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PersonControler>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+
+
+        {
+        }
+    }
+}
---
+++ LAB1/WebApplication3/Program.cs
@@ -0,0 +1,47 @@
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{ 
+    app.UseSwagger(c =>
+    {
+        c.SerializeAsV2 = true;
+    });
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = string.Empty;
+    });
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+app.UseSwagger(c =>
+{
+    c.SerializeAsV2 = true;
+});
+
+
+// Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),  
+// specifying the Swagger JSON endpoint.  
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.RoutePrefix = string.Empty;
+});
